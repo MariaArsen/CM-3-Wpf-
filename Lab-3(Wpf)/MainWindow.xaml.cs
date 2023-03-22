cbd2913c1887df4a72729bd7ed88edb2b81e4689
Maria_Arsen <maria_m_84@inbox.ru>
2023-03-22 15:34:17 +0300

    доблен словарь ресурсов

--- Lab-3(Wpf)/MainWindow.xaml.cs
+++ Lab-3(Wpf)/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace Lab_3_Wpf_
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontName = ((sender as ComboBox).SelectedItem as string);
             if (textBox != null)
             {
                 textBox.FontFamily = new FontFamily(fontName);
@@ -41,7 +41,7 @@ namespace Lab_3_Wpf_
         {
             if (textBox != null)
             {
-                double fontSize=Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+                double fontSize=Convert.ToDouble((sender as ComboBox).SelectedItem);
                 textBox.FontSize = fontSize;
             }
         }
