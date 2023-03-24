32dd7229b055029fdd49e0e7e40c6a115584538c
Maria_Arsen <maria_m_84@inbox.ru>
2023-03-24 13:44:38 +0300

    Внесена темная и светлая тема. Добавлена Ветвь СМ-9

--- Lab-3(Wpf)/MainWindow.xaml.cs
+++ Lab-3(Wpf)/MainWindow.xaml.cs
@@ -122,5 +122,13 @@ namespace Lab_3_Wpf_
         {
             Application.Current.Shutdown();
         }
+
+        private void theme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Uri theme = new Uri(themes.SelectedIndex == 0 ? "Light.xaml" : "Dark.xaml", UriKind.Relative);
+            ResourceDictionary themeDict = Application.LoadComponent(theme) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(themeDict);
+        }
     }
 }
